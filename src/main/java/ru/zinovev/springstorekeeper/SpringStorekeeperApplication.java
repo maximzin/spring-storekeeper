3b1b58a0b87852d41ce845a3b320e72fb632b15c
maxim zinovev <maximzin@mail.ru>
2024-04-09 21:36:03 +0500

    First commit

---
+++ src/main/java/ru/zinovev/springstorekeeper/SpringStorekeeperApplication.java
@@ -0,0 +1,13 @@
+package ru.zinovev.springstorekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStorekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStorekeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zinovev/springstorekeeper/SpringStorekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zinovev.springstorekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStorekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
