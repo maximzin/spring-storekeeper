2ef9fe0c342ed61e6326ec07414e7576c8d5ab93
maxim zinovev <maximzin@mail.ru>
2024-04-24 21:13:41 +0500

    Продолжение frontend

--- src/main/resources/static/js/deleteConfirm.js
+++ src/main/resources/static/js/deleteConfirm.js
@@ -1,10 +1,8 @@
-const deleteBtn = document.getElementsByClassName("formButtonHidden");
-const concrDeleteBtn = deleteBtn.item(0);
-const pressingBtn = document.getElementsByClassName("formButton");
-const concrPressBtn = pressingBtn.item(0);
-concrPressBtn.addEventListener("click", () => {
+const deleteBtn = document.getElementById("btnHidden");
+const pressBtn = document.getElementById("btnPressed");
+pressBtn.addEventListener("click", () => {
     const result = confirm("Подтвердите удаление, будут удалены все связанные объекты");
     if (result===true) {
-        concrDeleteBtn.click();
+        deleteBtn.click();
     }
 });
